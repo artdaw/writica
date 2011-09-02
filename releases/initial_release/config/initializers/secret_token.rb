91acd4c743dd272ec289f02b02b156d296aa6ccc
Gleb <artdaw@gmail.com>
2011-09-02 17:40:07 +0300

    project initial structure and prototype

---
+++ releases/initial_release/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+InitialRelease::Application.config.secret_token = '2c86004c3dfabe9825bc18854f9fa56600b8f5cfa8dd40263ca8650473309c7704cfe0b4210bd90cec28ad5624daa1e1060db41f6f938832c9b3e571582d6001'
